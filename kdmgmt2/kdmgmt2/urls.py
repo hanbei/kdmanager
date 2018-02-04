c24ebc0c10c83b35cd6407574113631016024582
Florian Schulz <hanbei@hanbei.de>
2018-02-04 22:05:45 +0100

    Move kader toplevel

--- kdmgmt2/kdmgmt2/urls.py
+++ kdmgmt2/kdmgmt2/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('kader/', include('kader.urls')),
+    path('', include('kader.urls')),
     path('admin/', admin.site.urls),
 ]
